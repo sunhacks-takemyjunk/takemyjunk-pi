81ca179ed44bb7760630c1048c60b7247343175a
jjin43 <jjin43@asu.edu>
2024-10-02 12:08:29 -0700

    states

--- run_pi.py
+++ run_pi.py
@@ -3,26 +3,28 @@ import ssl
 import json
 import logging
 
-# Enable logging
-logging.basicConfig(level=logging.DEBUG)
-
-# Define the MQTT client
-client = mqtt.Client()
+state = 0
 
 # Define the callback for when a message is received
 def on_message(client, userdata, message):
     payload = json.loads(message.payload)
     action = payload.get('action')
     if action == "open_door":
-        # Perform the hardware action here
-        print("Opening the door")
+        if state == 0:
+            # Perform the hardware action here
+            print("Opening the door")
+            state = 1
+        else:
+            print("Door is already open")
     elif action == "close_door":
-        # Perform the hardware action here
-        print("Closing the door")
-
-# Set the callback for message reception
-client.on_message = on_message
-
+        if state == 1:
+            # Perform the hardware action here
+            print("Closing the door")
+            state = 0
+        else:
+            print("Door is already closed")
+        
+        
 # Define the callback for connection
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -30,21 +32,33 @@ def on_connect(client, userdata, flags, rc):
         client.subscribe("pi_tasks")
     else:
         print(f"Connection failed with code {rc}")
+        
+        
+        
+if __name__ == "__main__":    
+    # Enable logging
+    logging.basicConfig(level=logging.DEBUG)
 
-# Set the callback for connection
-client.on_connect = on_connect
+    # Define the MQTT client
+    client = mqtt.Client()
+    
+    # Set the callback for message reception
+    client.on_message = on_message
 
-# Configure the client with the AWS IoT Core endpoint and certificates
-client.tls_set(ca_certs="root-CA.crt",
-               certfile="takemyjunk_thing.cert.pem",
-               keyfile="takemyjunk_thing.private.key",
-               tls_version=ssl.PROTOCOL_TLSv1_2,
-               ciphers=None)
+    # Set the callback for connection
+    client.on_connect = on_connect
 
-# Connect to the AWS IoT Core endpoint
-print("Connecting to AWS IoT Core endpoint...")
-client.connect("at6fo26c2tah4-ats.iot.us-east-1.amazonaws.com", port=8883)
+    # Configure the client with the AWS IoT Core endpoint and certificates
+    client.tls_set(ca_certs="root-CA.crt",
+                certfile="takemyjunk_thing.cert.pem",
+                keyfile="takemyjunk_thing.private.key",
+                tls_version=ssl.PROTOCOL_TLSv1_2,
+                ciphers=None)
 
-# Start the MQTT client
-print("Starting MQTT client loop...")
-client.loop_forever()
+    # Connect to the AWS IoT Core endpoint
+    print("Connecting to AWS IoT Core endpoint...")
+    client.connect("at6fo26c2tah4-ats.iot.us-east-1.amazonaws.com", port=8883)
+
+    # Start the MQTT client
+    print("Starting MQTT client loop...")
+    client.loop_forever()
